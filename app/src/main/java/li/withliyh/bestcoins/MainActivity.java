c9efaafc4739bd2489fa3762280131bbc1f97850
withliyh <withliyh@gmail.com>
2015-05-17 12:25:18 +0800

    增加左边滑动

--- app/src/main/java/li/withliyh/bestcoins/MainActivity.java
+++ app/src/main/java/li/withliyh/bestcoins/MainActivity.java
@@ -1,17 +1,59 @@
 package li.withliyh.bestcoins;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private View mDrawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+       // setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        //getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_drawer);
+        mDrawer = findViewById(R.id.my_awesome_drawer);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.my_drawer_layout);
+        //mDrawerLayout.setStatusBarBackgroundColor();
+        //mDrawerLayout.openDrawer(drawer);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+                R.string.drawer_open,R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                getSupportActionBar().setTitle("Home");
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle("Nav");
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     @Override
@@ -22,10 +64,17 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean open = mDrawerLayout.isDrawerOpen(mDrawer);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        if (mDrawerToggle.onOptionsItemSelected(item)) return true;
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -34,5 +83,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
